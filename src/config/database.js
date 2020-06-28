e8403d71030231a9e7ec569c1dfd624efe67fbe7
moroniunopar <45543104+moroniunopar@users.noreply.github.com>
2020-06-28 10:59:38 -0300

    domingo

--- src/config/database.js
+++ src/config/database.js
@@ -3,17 +3,17 @@
 module.exports = {
      dialect: 'mysql',
 
-    // host: 'localhost',
-    // username: 'root',
-    // password: '',
-    // //password: '123456',
-    // database: 'teste001',
+    host: 'localhost',
+    username: 'root',
+    password: '',
+    //password: '123456',
+    database: 'teste001',
 
-       host: 'mysql669.umbler.com',
-       port: 41890,
-       username: 'sqlallex',
-       password: 'SQL123456',
-       database: 'moroniteste',
+    //    host: 'mysql669.umbler.com',
+    //    port: 41890,
+    //    username: 'sqlallex',
+    //    password: 'SQL123456',
+    //    database: 'moroniteste',
 
     define: {
         timestamps: true,   // todas as tabelas terão  createdAt & updateAt
